ed929cfb6687cd5fc7671ace90797b2c10a021fc
Richard Nwankwo <rich.nwan@gmail.com>
2016-02-03 09:13:58 -0500

    Portfolio Site Initial Upload

---
+++ resources/views/projects.php
@@ -0,0 +1,49 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>projects</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="./embed/bootstrap/superhero/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather" />
+    <link rel="stylesheet" href="./embed/bootstrap/fonts/font-awesome.min.css" />
+</head>
+
+<body>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center">Projects</h1></div>
+        </div>
+        <div class="row">
+            <div class="col-xs-10 centering">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Gladys: Idea Creating Tool</h3></div>
+                    <div class="panel-body"><span>Gladys is a side project I worked on to help me brainstorm.. Whenever I read anything interesting I pop it into Gladys. When I want to revist a topic, Gladys creates questions to get my brain juices flowing on the topic. Gladys was built with Laravel and Angular 1.x</span></div>
+                    <div
+                        class="panel-footer"><span class="text-lowercase text-primary">Github.com/Gladys</span></div>
+                </div>
+            </div>
+            <div class="col-md-12 text-center">
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="list-inline mainMenu">
+                            <li><a href="#">About </a></li>
+                            <li><a href="#">Blog </a></li>
+                            <li><a href="#">Contact </a></li>
+                            <li><a href="#">Projects </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
