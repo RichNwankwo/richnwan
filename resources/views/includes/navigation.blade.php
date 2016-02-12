bf1c41709f5749851b68ff6299eced76926350f9
Richard Nwankwo <rich.nwan@gmail.com>
2016-02-12 07:09:53 -0500

    Completed PJAX implementation

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -1,10 +1,10 @@
 <div class="row">
     <div class="col-md-12 text-center">
         <ul class="list-inline mainMenu">
-            <li><a href="/">Home </a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/projects">Projects </a></li>
+            <li><a data-pjax href="/">Home </a></li>
+            <li><a data-pjax href="/about">About</a></li>
+            <li><a data-pjax href="/contact">Contact</a></li>
+            <li><a data-pjax href="/projects">Projects </a></li>
         </ul>
     </div>
 </div>
